14d20ad230dce161fe320ec769847599c8268a48
olehmyhal <olehmyhal.pers@gmail.com>
2022-05-05 12:59:00 +0300

    app done

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ShoppingListModule,
+    SharedModule,
+    CoreModule,
+    AuthModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
